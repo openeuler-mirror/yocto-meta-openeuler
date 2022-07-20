fb533edc70c7d90fffc53c1e5ac89b53e5caadff
fanglinxu <fanglinxu@huawei.com>
2022-07-20 16:48:37 +0800

    yocto: solve libmnl download bug
    * download libmnl, add by download_code.sh
    
    Signed-off-by: fanglinxu <fanglinxu@huawei.com>

--- scripts/download_code.sh
+++ scripts/download_code.sh
@@ -206,6 +206,7 @@ download_code()
     update_code_repo src-openeuler/coreutils ${SRC_BRANCH}
     update_code_repo src-openeuler/findutils ${SRC_BRANCH}
     update_code_repo src-openeuler/gawk ${SRC_BRANCH}
+    update_code_repo src-openeuler/libmnl ${SRC_BRANCH}
 }
 
 # download iSulad related packages
