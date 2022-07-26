91ceea95df2b966fa9070d12476fdd7db776587a
fanglinxu <fanglinxu@huawei.com>
2022-07-26 11:19:19 +0800

    yocto: add bison download_code.sh
    * download bison
    
    Signed-off-by: fanglinxu <fanglinxu@huawei.com>

--- scripts/download_code.sh
+++ scripts/download_code.sh
@@ -212,6 +212,7 @@ download_code()
     update_code_repo src-openeuler/libuv ${SRC_BRANCH}
     update_code_repo src-openeuler/flex ${SRC_BRANCH}
     update_code_repo src-openeuler/sqlite ${SRC_BRANCH}
+    update_code_repo src-openeuler/bison ${SRC_BRANCH}
 }
 
 # download iSulad related packages
