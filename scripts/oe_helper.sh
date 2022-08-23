924e781135935b10edbe2144052f2a1fc5b522a8
hanzongcheng <hanzongcheng@huawei.com>
2022-08-23 15:44:13 +0800

    yocto: add build option for PREEMPT_RT kernel
    * Specify the default value of "PREFERRED_PROVIDER_virtual/kernel" in local.conf
    * Add "--enable-rt" option to oe-helper.sh to help us build the PREEMPT_RT kernel
    
    Signed-off-by: hanzongcheng <hanzongcheng@huawei.com>

--- scripts/oe_helper.sh
+++ scripts/oe_helper.sh
@@ -8,7 +8,7 @@ usage () {
 cat << EOF
 Usage: 
 download mode: source $script [-D] [-d DOWNLOAD_DIR] <-b BRANCH> <-m MANIFEST_FILE>
-compile mode: source $script [-C] [-p PLATFORM] [-o BUILD_DIR] <-t TOOLCHAIN_DIR>  <-i INIT_MANAGER>
+compile mode: source $script [-C] [-p PLATFORM] [-o BUILD_DIR] <-t TOOLCHAIN_DIR>  <-i INIT_MANAGER> <--enable-rt>
   [] -- need   <> -- Optional
 -------------------------------------------------------
   -h                show this help and exit.
@@ -33,6 +33,7 @@ compile mode: source $script [-C] [-p PLATFORM] [-o BUILD_DIR] <-t TOOLCHAIN_DIR
   -i INIT_MANAGER   INIT_MANAGER suooprt:
                         busybox (defaut)
                         systemd
+  --enable-rt       Enable PREEMPT_RT kernel
 EOF
 }                                                                            
 
@@ -60,11 +61,22 @@ check_cmd_source () {
 check_cmd_source || exit 1
 compile_mode=0
 download_mode=0
+ENABLE_PREEMPT_RT=0
 INIT_MANAGER="busybox"
 OPTIND=1
 
-while getopts "hDCSd:b:m:p:o:t:i:" opt; do
+while getopts "hDCSd:b:m:p:o:t:i:-:" opt; do
     case $opt in
+        -)
+            case "${OPTARG}" in
+                enable-rt) ENABLE_PREEMPT_RT=1
+                           echo "Note: enable PREEMPT_RT."
+                           ;;
+                *)  usage
+                    return 1
+                    ;;
+            esac
+            ;;
         h)  usage
             return 0
             ;;
@@ -122,6 +134,9 @@ if [ $compile_mode == 1 ]; then
         return 1
     fi
     source ${script_dir}/compile.sh ${PLATFORM} ${BUILD_DIR} ${TOOLCHAIN_DIR}
+    if [ $ENABLE_PREEMPT_RT == 1 ]; then
+        sed -i "s|^PREFERRED_PROVIDER_virtual/kernel .*|PREFERRED_PROVIDER_virtual/kernel = \"linux-openeuler-rt\"|g" conf/local.conf
+    fi
     case $INIT_MANAGER in
     "busybox")
         sed -i "s|^OPENEULER_INIT_MANAGER .*|OPENEULER_INIT_MANAGER = \"mdev-busybox\"|g" conf/local.conf
