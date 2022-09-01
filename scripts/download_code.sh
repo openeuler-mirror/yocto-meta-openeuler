6a9faef99173ab9ea21c82885843b8e8aee3536a
YanqiXu <y449xu@uwaterloo.ca>
2022-09-01 08:37:41 +0000

    yocto: add bbappend for xorgproto
    * add xorgproto 2021.05 from src-openeuler/xorg-x11-proto-devel
    * this package is required by mesa in build stage
    * add src-openeuler/xorg-x11-proto-devel to download
    
    Signed-off-by: YanqiXu <y449xu@uwaterloo.ca>

--- scripts/download_code.sh
+++ scripts/download_code.sh
@@ -245,6 +245,7 @@ download_code()
     update_code_repo src-openeuler/uthash ${SRC_BRANCH}
     update_code_repo src-openeuler/ppp ${SRC_BRANCH}
     update_code_repo src-openeuler/libdrm ${SRC_BRANCH}
+    update_code_repo src-openeuler/xorg-x11-proto-devel ${SRC_BRANCH}
     update_code_repo src-openeuler/mtd-utils openEuler-22.03-LTS
     update_code_repo src-openeuler/dtc ${SRC_BRANCH}
 }
