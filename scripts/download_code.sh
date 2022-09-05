0847e9514e55fdf563ecfb3d89292a8429fed1f7
hanzongcheng <hanzongcheng@huawei.com>
2022-09-05 20:16:16 +0800

    kernel: update to openEuler 5.10.0-106.8.0
    * The current PREEMPT_RT patches is based on openEuler 5.10.0-106.8.0,
      so update kernel version to openEuler 5.10.0-106.8.0
    
    Signed-off-by: hanzongcheng <hanzongcheng@huawei.com>

--- scripts/download_code.sh
+++ scripts/download_code.sh
@@ -302,7 +302,7 @@ main()
     SRC_BRANCH="$2"
     # manifest file include the git url, revision, path info
     MANIFEST="$3"
-    KERNEL_BRANCH="5.10.0-106.6.0"
+    KERNEL_BRANCH="5.10.0-106.8.0"
 
     check_use || return 1
     set -e
