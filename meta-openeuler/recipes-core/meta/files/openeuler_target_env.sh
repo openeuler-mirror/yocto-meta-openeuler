fcb8f2ba59b71bd4eb576ef8d6a6deb0798c9311
hanzongcheng <hanzongcheng@huawei.com>
2022-10-19 15:03:03 +0800

    sdk: unset PKG_CONFIG when making modules_prepare
    * Fixes compilation error:
       /usr/bin/ld: cannot find /lib64/libc.so.6: No such file or directory
       /usr/bin/ld: cannot find /usr/lib64/libc_nonshared.a: No such file or directory
       collect2: error: ld returned 1 exit status
    
      This error occurs because we use PKG_CONFIG when making modules_prepare. When
      compiling extract-cert, pkg-config will add "-L${sysroot}/usr/lib64 -lcrypto".
      "${sysroot}/usr/lib64" includes a link script for libc.so, it points to "/lib64/libc.so.6",
      but on ubuntu the libc.so is installed in "/lib/${arch}/libc.so". To fix this problem,
      we just unset PKGCONFIG when making modules_prepare. This is may not quite right,
      but it is a quick fix.
    
    Signed-off-by: hanzongcheng <hanzongcheng@huawei.com>

--- meta-openeuler/recipes-core/meta/files/openeuler_target_env.sh
+++ meta-openeuler/recipes-core/meta/files/openeuler_target_env.sh
@@ -1,5 +1,5 @@
 export KERNEL_SRC_DIR="${SDKTARGETSYSROOT}/usr/src/kernel"
 # prepare context for kernel module development
 pushd "${SDKTARGETSYSROOT}/usr/src/kernel"
-make modules_prepare
+make modules_prepare PKG_CONFIG_SYSROOT_DIR= PKG_CONFIG_PATH=
 popd
