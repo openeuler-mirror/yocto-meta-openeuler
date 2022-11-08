openeuler-mirror/yocto-meta-openeuler

commit e7b7270033f44e0805d47d580e98b5b9d80051d1

yocto: add ethercat-e1000e-5.10
 * add ethercat-e1000e-5.10.tar.gz
 * add patch to fix ethercat tool compilation
 * add recipe for ethercat

Signed-off-by: hanzongcheng <hanzongcheng@huawei.com>

meta-openeuler/recipes-support/ethercat/ethercat_e1000e-5.10.bb
@@ -1,0 +1,48 @@
+DESCRIPTION = "IgH EtherCAT Master for Linux"
+HOMEPAGE = "http://etherlab.org/download/ethercat"
+LICENSE = "GPL-2.0 & LGPL-2.1"
+LIC_FILES_CHKSUM = "file://COPYING;md5=59530bdf33659b29e73d4adb9f9f6552"
+SECTION = "net"
+
+PV = "e1000e-5.10"
+SRC_URI = "file://ethercat-${PV}.tar.gz \
+	   file://0001-ethercat-Fix-ethercat-tool-compilation.patch \
+	   "
+
+PACKAGECONFIG ??= "e1000e"
+
+PACKAGECONFIG[generic] = "--enable-generic,--disable-generic,"
+PACKAGECONFIG[8139too] = "--enable-8139too,--disable-8139too,"
+PACKAGECONFIG[e100]    = "--enable-e100,--disable-e100,"
+PACKAGECONFIG[e1000]   = "--enable-e1000,--disable-e1000,"
+PACKAGECONFIG[e1000e]  = "--enable-e1000e,--disable-e1000e,"
+PACKAGECONFIG[r8169]   = "--enable-r8169,--disable-r8169,"
+
+do_configure[depends] += "virtual/kernel:do_shared_workdir"
+
+inherit autotools-brokensep pkgconfig module-base
+
+# disable the textrel check
+INSANE_SKIP_${PN} = "textrel"
+
+EXTRA_OECONF += "--with-linux-dir=${STAGING_KERNEL_BUILDDIR}"
+EXTRA_OECONF += "--with-module-dir=kernel/ethercat"
+
+do_configure_prepend() {
+	# Do distclean before configure to fix the error in the second compilation:
+	# | error: .libs/libethercat_la-common.o: No such file or directory
+	oe_runmake distclean
+	# Fixes configure error
+	# | Makefile.am: error: required file './ChangeLog' not found"
+	touch ChangeLog
+}
+
+do_compile_append() {
+	oe_runmake modules
+}
+
+do_install_append() {
+	oe_runmake MODLIB=${D}${nonarch_base_libdir}/modules/${KERNEL_VERSION} modules_install
+}
+
+FILES_${PN} += "${nonarch_base_libdir}/modules/${KERNEL_VERSION}"

meta-openeuler/recipes-core/packagegroups/packagegroup-base.bb
@@ -28,6 +28,7 @@ curl \
 dosfstools \
 e2fsprogs \
 e2fsprogs-tune2fs \
+ethercat \
 ethtool \
 expat \
 grep \

meta-openeuler/recipes-support/ethercat/files/0001-ethercat-Fix-ethercat-tool-compilation.patch
@@ -1,0 +1,73 @@
+From 0ad148040da972a977d2cb7f8b0a1af7a3df038d Mon Sep 17 00:00:00 2001
+From: hanzongcheng <hanzongcheng@huawei.com>
+Date: Mon, 10 Oct 2022 05:05:42 +0800
+Subject: [PATCH] ethercat: Fix ethercat tool compilation
+
+* After commit f3ccfda193197 ("ethtool: extend coalesce setting uAPI with CQE mode") and
+  commit 7462494408cd3 ("ethtool: extend ringparam setting/getting API with rx_buf_len"),
+  ethtool extend the setting/getting API, it causes the following compilation error:
+
+    ethtool-5.10-ethercat.c: error: initialization of '...' from incompatible pointer type '...' [-Werror=incompatible-pointer-types]
+    .get_ringparam  = e1000_get_ringparam,
+                      ^~~~~~~~~~~~~~~~~~~
+    .set_ringparam  = e1000_set_ringparam,
+                      ^~~~~~~~~~~~~~~~~~~
+    .get_coalesce  = e1000_get_coalesce,
+                     ^~~~~~~~~~~~~~~~~~
+    .set_coalesce  = e1000_set_coalesce,
+                     ^~~~~~~~~~~~~~~~~~
+  Modify these function arguments to fix this error.
+
+Signed-off-by: hanzongcheng <hanzongcheng@huawei.com>
+
+diff --git a/devices/e1000e/ethtool-5.10-ethercat.c b/devices/e1000e/ethtool-5.10-ethercat.c
+index 3e71855e..c03eff67 100644
+--- a/devices/e1000e/ethtool-5.10-ethercat.c
++++ b/devices/e1000e/ethtool-5.10-ethercat.c
+@@ -655,7 +655,9 @@ static void e1000_get_drvinfo(struct net_device *netdev,
+ }
+ 
+ static void e1000_get_ringparam(struct net_device *netdev,
+-				struct ethtool_ringparam *ring)
++				struct ethtool_ringparam *ring,
++				struct kernel_ethtool_ringparam *kernel_ring,
++				struct netlink_ext_ack *extack)
+ {
+ 	struct e1000_adapter *adapter = netdev_priv(netdev);
+ 
+@@ -666,7 +668,9 @@ static void e1000_get_ringparam(struct net_device *netdev,
+ }
+ 
+ static int e1000_set_ringparam(struct net_device *netdev,
+-			       struct ethtool_ringparam *ring)
++			       struct ethtool_ringparam *ring,
++			       struct kernel_ethtool_ringparam *kernel_ring,
++			       struct netlink_ext_ack *extack)
+ {
+ 	struct e1000_adapter *adapter = netdev_priv(netdev);
+ 	struct e1000_ring *temp_tx = NULL, *temp_rx = NULL;
+@@ -1991,7 +1995,9 @@ static int e1000_set_phys_id(struct net_device *netdev,
+ }
+ 
+ static int e1000_get_coalesce(struct net_device *netdev,
+-			      struct ethtool_coalesce *ec)
++			      struct ethtool_coalesce *ec,
++			      struct kernel_ethtool_coalesce *kernel_coal,
++			      struct netlink_ext_ack *extack)
+ {
+ 	struct e1000_adapter *adapter = netdev_priv(netdev);
+ 
+@@ -2004,7 +2010,9 @@ static int e1000_get_coalesce(struct net_device *netdev,
+ }
+ 
+ static int e1000_set_coalesce(struct net_device *netdev,
+-			      struct ethtool_coalesce *ec)
++			      struct ethtool_coalesce *ec,
++			      struct kernel_ethtool_coalesce *kernel_coal,
++			      struct netlink_ext_ack *extack)
+ {
+ 	struct e1000_adapter *adapter = netdev_priv(netdev);
+ 
+-- 
+2.34.1
+

commit d3d85363806aa361c06be2d415896dc58830a273

ethercat: Adjusting the order of distclean
* In order to keep a clean build environment, we executed distclean
  before do_configure. But distclean is dependent on do_configure,
  doing distclean directly before do_configure will fail on the first
  compilation. Therefore, do distclean after install is complete.

Fixes: e7b7270033f44 ("yocto: add ethercat-e1000e-5.10")
Signed-off-by: hanzongcheng <hanzongcheng@huawei.com>

meta-openeuler/recipes-support/ethercat/ethercat_e1000e-5.10.bb
@@ -29,9 +29,6 @@ EXTRA_OECONF += "--with-linux-dir=${STAGING_KERNEL_BUILDDIR}"
 EXTRA_OECONF += "--with-module-dir=kernel/ethercat"
 
 do_configure_prepend() {
-	# Do distclean before configure to fix the error in the second compilation:
-	# | error: .libs/libethercat_la-common.o: No such file or directory
-	oe_runmake distclean
 	# Fixes configure error
 	# | Makefile.am: error: required file './ChangeLog' not found"
 	touch ChangeLog
@@ -43,6 +40,9 @@ do_compile_append() {
 
 do_install_append() {
 	oe_runmake MODLIB=${D}${nonarch_base_libdir}/modules/${KERNEL_VERSION} modules_install
+	# Do distclean after installation to fix the error in the second compilation:
+	# | error: .libs/libethercat_la-common.o: No such file or directory
+	oe_runmake distclean
 }
 
 FILES_${PN} += "${nonarch_base_libdir}/modules/${KERNEL_VERSION}"