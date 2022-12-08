openeuler-mirror/yocto-meta-openeuler

commit 6574cb47744e7beeeaa8eab6f44f62f9b5b0ccc0

kernel: update to openEuler 5.10.0-132.0.0
* Update kernel to openEuler 5.10.0-132.0.0, checkout src-kernel
  to "openEuler-22.03-LTS-SP1" and apply the corresponding rt patch.

Signed-off-by: hanzongcheng <hanzongcheng@huawei.com>

bsp/meta-openeuler-bsp/raspberrypi/recipes-kernel/linux/linux-openeuler-rt.bbappend
@@ -1,13 +1,13 @@
 # For the Raspberry Pi, we don't need to apply the aarch64 patches
 SRC_URI_remove_raspberrypi4 += " \
-    file://src-kernel-5.10/0000-kernel-rt62.patch \
-    file://src-kernel-5.10/0001-kernel-rt62-modify-defconfig.patch \
+    file://src-kernel-5.10/0001-apply-preempt-RT-patch.patch \
+    file://src-kernel-5.10/0001-modify-openeuler_defconfig-for-rt62.patch \
 "
 
 SRC_URI += "\
     file://src-kernel-5.10/0000-raspberrypi-kernel.patch \
-    file://src-kernel-5.10/0001-raspberrypi-kernel-rt62.patch \
-    file://src-kernel-5.10/0002-raspberrypi-kernel-rt62-modify-defconfig.patch \
+    file://src-kernel-5.10/0001-apply-preempt-RT-patch.patch
+    file://src-kernel-5.10/0002-modify-bcm2711_defconfig-for-rt-rpi-kernel.patch
 "
 
 COMPATIBLE_MACHINE = "raspberrypi4-64"

meta-openeuler/recipes-kernel/linux/linux-openeuler-rt.bb
@@ -9,13 +9,13 @@ SRC_URI = " \
 "
 
 SRC_URI_append_aarch64 += " \
-    file://src-kernel-5.10/0000-kernel-rt62.patch \
-    file://src-kernel-5.10/0001-kernel-rt62-modify-defconfig.patch \
+    file://src-kernel-5.10/0001-apply-preempt-RT-patch.patch \
+    file://src-kernel-5.10/0001-modify-openeuler_defconfig-for-rt62.patch \
 "
 
 SRC_URI_append_x86-64 += " \
-    file://src-kernel-5.10/0000-kernel-rt62.patch \
-    file://src-kernel-5.10/0001-kernel-rt62-modify-defconfig.patch \
+    file://src-kernel-5.10/0001-apply-preempt-RT-patch.patch \
+    file://src-kernel-5.10/0001-modify-openeuler_defconfig-for-rt62.patch \
 "
 
 S = "${WORKDIR}/kernel-5.10"

meta-openeuler/recipes-kernel/linux/linux-openeuler.inc
@@ -8,13 +8,6 @@ SRC_URI_append_aarch64 += " \
     file://yocto-embedded-tools/patches/${ARCH}/0001-arm64-add-zImage-support-for-arm64.patch \
 "
 
-# add patches for OPENEULER_PLATFROM such as aarch64-pro
-SRC_URI_append_aarch64-pro += " \
-    file://src-kernel-5.10/0000-raspberrypi-kernel.patch \
-    file://src-kernel-5.10/0001-add-preemptRT-patch.patch \
-    file://src-kernel-5.10/0002-modifty-bcm2711_defconfig-for-rt-rpi-kernel.patch \
-"
-
 LINUX_VERSION_EXTENSION_append = "-openeuler"
 #delete v8 in kernel module name, such as kernel-module-xxx-5.10.0-v8
 KERNEL_MODULE_PACKAGE_SUFFIX = ""

scripts/download_code.sh
@@ -113,7 +113,7 @@ download_code()
     # add new package here if required
     rm -f "${SRC_DIR}"/code.list
     update_code_repo openeuler/kernel ${KERNEL_BRANCH} kernel-5.10
-    update_code_repo src-openeuler/kernel openEuler-22.03-LTS-Next src-kernel-5.10
+    update_code_repo src-openeuler/kernel ${SRC_BRANCH} src-kernel-5.10
     update_code_repo src-openeuler/busybox ${SRC_BRANCH}
     update_code_repo openeuler/dsoftbus_standard v3.1
     update_code_repo src-openeuler/libboundscheck ${SRC_BRANCH}
@@ -303,7 +303,7 @@ main()
     SRC_BRANCH="$2"
     # manifest file include the git url, revision, path info
     MANIFEST="$3"
-    KERNEL_BRANCH="5.10.0-129.0.0"
+    KERNEL_BRANCH="5.10.0-132.0.0"
 
     check_use || return 1
     set -e

commit b46098ecada1960699e9ea861471ce93f3aa5631

linux-rt: adapt the preempt-rt to raspberrypi
 * "0001-apply-preempt-RT-patch.patch" is already configured in meta-openeuler,
   so there is no need to use it again in the raspberrypi layer.

Fixes: 6574cb47744e7 ("kernel: update to openEuler 5.10.0-132.0.0")
Signed-off-by: hanzongcheng <hanzongcheng@huawei.com>

bsp/meta-openeuler-bsp/raspberrypi/recipes-kernel/linux/linux-openeuler-rt.bbappend
@@ -1,12 +1,10 @@
 # For the Raspberry Pi, we don't need to apply the aarch64 patches
 SRC_URI_remove_raspberrypi4 += " \
-    file://src-kernel-5.10/0001-apply-preempt-RT-patch.patch \
     file://src-kernel-5.10/0001-modify-openeuler_defconfig-for-rt62.patch \
 "
 
 SRC_URI += "\
     file://src-kernel-5.10/0000-raspberrypi-kernel.patch \
-    file://src-kernel-5.10/0001-apply-preempt-RT-patch.patch \
     file://src-kernel-5.10/0002-modify-bcm2711_defconfig-for-rt-rpi-kernel.patch \
 "
 