fe8046bfbea2c76f1b6292eb88e4c3f476e4c4f5
fanglinxu <fanglinxu@huawei.com>
2023-06-26 21:00:45 +0800

    sdk: fix kernel source tree initialization bug
    * Fix the initialization bug in the kernel source tree
      when developing SDK using the nativesdk container
      environment.
    
    * Optimize script code.
    
    Signed-off-by: fanglinxu <fanglinxu@huawei.com>

--- meta-openeuler/recipes-core/meta/files/openeuler_target_env.sh
+++ meta-openeuler/recipes-core/meta/files/openeuler_target_env.sh
@@ -1,15 +1,22 @@
 export KERNEL_SRC="${SDKTARGETSYSROOT}/usr/src/kernel"
-# prepare context for kernel module development
-pushd "${SDKTARGETSYSROOT}/usr/src/kernel"
-make modules_prepare PKG_CONFIG_SYSROOT_DIR= PKG_CONFIG_PATH=
-popd
-
-# prepare ROS SDK, only works in the ROS environment with our nativesdk.
+OPENEULER_NATIVESDK_SYSROOT=/opt/buildtools/nativesdk/sysroots/x86_64-pokysdk-linux
+# prepare SDK
 PYTHONBIN=`which python3`
 PYTHONPKGPATH="${PYTHONBIN%/*}/../lib/python3.9/site-packages/"
-if [ ${PYTHONPKGPATH#/opt/buildtools/nativesdk/sysroots/x86_64-pokysdk-linux} != "$PYTHONPKGPATH" ]; then
+if [ ${PYTHONPKGPATH#${OPENEULER_NATIVESDK_SYSROOT}} != "$PYTHONPKGPATH" ]; then
+    # prepare context for kernel module development when using nativesdk
+    pushd "${SDKTARGETSYSROOT}/usr/src/kernel"
+    make modules_prepare PKG_CONFIG_SYSROOT_DIR= PKG_CONFIG_PATH= KBUILD_HOSTCFLAGS+="-L${OPENEULER_NATIVESDK_SYSROOT}/usr/lib \
+                            -L${OPENEULER_NATIVESDK_SYSROOT}/lib \
+                            -Wl,-rpath-link,${OPENEULER_NATIVESDK_SYSROOT}/usr/lib \
+                            -Wl,-rpath-link,${OPENEULER_NATIVESDK_SYSROOT}/lib \
+                            -Wl,-rpath,${OPENEULER_NATIVESDK_SYSROOT}/usr/lib \
+                            -Wl,-rpath,${OPENEULER_NATIVESDK_SYSROOT}/lib"
+    popd
+
     if test -d $PYTHONPKGPATH; then
         if test -d ${SDKTARGETSYSROOT}/usr/share/rosidl_cmake; then
+            # prepare ROS SDK, only works in the ROS environment with our nativesdk.
             pushd $PYTHONPKGPATH
             # Here, we have a symbolic link for the Python package that is used by the ROS package
             # in the lib directory of our sysroot environment, instead of lib64.
@@ -19,14 +26,20 @@ if [ ${PYTHONPKGPATH#/opt/buildtools/nativesdk/sysroots/x86_64-pokysdk-linux} !=
                 ln -sfnT "$file" "$(basename "$file")"
             done
             popd
+
             # Install host python tools
             python3 -m pip install -r $OECORE_NATIVE_SYSROOT/environment-setup.d/requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
             export CMAKE_TOOLCHAIN_FILE="$OECORE_NATIVE_SYSROOT/environment-setup.d/toolchain.cmake"
-            # avoid pythonpath err in colcon
-            sed -i 's%python_path.exists():%python_path.exists() and not (\"\%s\" \% python_path).startswith("/opt/buildtools/nativesdk/sysroots/x86_64-pokysdk-linux/usr/lib"):%' /opt/buildtools/nativesdk/sysroots/x86_64-pokysdk-linux/usr/lib/python3.*/site-packages/colcon_core/environment/pythonpath.py
+	    # avoid pythonpath err in colcon
+            sed -i 's%python_path.exists():%python_path.exists() and not (\"\%s\" \% python_path).startswith("/opt/buildtools/nativesdk/sysroots/x86_64-pokysdk-linux/usr/lib"):%' ${OPENEULER_NATIVESDK_SYSROOT}/usr/lib/python3.*/site-packages/colcon_core/environment/pythonpath.py
         fi
-        # add find_path cross compile support for numpy path
-        sed -i '/_NumPy_PATH/{n; s%NO_DEFAULT_PATH)%NO_DEFAULT_PATH CMAKE_FIND_ROOT_PATH_BOTH)%}' /opt/buildtools/nativesdk/sysroots/x86_64-pokysdk-linux/usr/share/cmake-*/Modules/FindPython/Support.cmake
+	# add find_path cross compile support for numpy path
+	sed -i '/_NumPy_PATH/{n; s%NO_DEFAULT_PATH)%NO_DEFAULT_PATH CMAKE_FIND_ROOT_PATH_BOTH)%}' ${OPENEULER_NATIVESDK_SYSROOT}/usr/share/cmake-*/Modules/FindPython/Support.cmake
     fi
+else
+    # prepare context for kernel module development
+    pushd "${SDKTARGETSYSROOT}/usr/src/kernel"
+    make modules_prepare PKG_CONFIG_SYSROOT_DIR= PKG_CONFIG_PATH=
+    popd
 fi
 
